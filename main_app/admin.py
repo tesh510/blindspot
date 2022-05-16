275a3cbe4c58b8d206cd0a776ec397c5b539f7c5
Jordan-Christopher Garcia <jctgarcia20@gmail.com>
2022-05-16 16:21:27 -0700

    Full-CRUD functionality

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Car
+from .models import Car, Review, Comment, Photo
 
 # Register your models here.
 
 admin.site.register(Car)
+admin.site.register(Review)
+admin.site.register(Comment)
+admin.site.register(Photo)
+
--- main_app/models.py
+++ main_app/models.py
@@ -1,18 +1,50 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.urls import reverse
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 
 class Car(models.Model):
-    make = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
-    year = models.IntegerField()
-    engine = models.CharField(max_length=50, default = 'v8')
-    mileage = models.CharField(max_length=50)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    def __str__(self):
-      return f'{self.make} ({self.id})'
-    def get_absolute_url(self):
-      return reverse('detail', kwargs={'car_id': self.id})
+  make = models.CharField(max_length=50)
+  model = models.CharField(max_length=50)
+  year = models.IntegerField()
+  engine = models.CharField(max_length=50)
+  mileage = models.CharField(max_length=50)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  # might cause error
+  reviews = models.ManyToManyField(Review)
 
+  def __str__(self):
+    return f'{self.make} ({self.id})'
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'car_id': self.id})
+
+class Review(models.Model):
+  name = models.CharField(max_length=1000)
+  date = models.DateField('review date')
+
+  def __str__(self):
+    return f'{self.make} ({self.id})'
+
+  def get_absolute_url(self):
+    return reverse('reviews_detail', kwargs={'pk': self.id})
+
+class Comment(models.Model):
+  description = models.CharField(max_length=150)
+  date = models.DateField('comment date')
+  car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.make} ({self.id})'
+
+  class Meta:
+    ordering = ['-date']
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for car_id: {self.car_id} @{self.url}"
